e0395ff1c0ad7e8b2dfd3bccaf4322a185bde673
bobby.liu <fellowe5@hotmail.com>
2026-03-24 08:15:26 +0800

    Add skeleton to query Merchant details by merchant name

--- src/main/java/com/gomcc/merchant_checker_service/repository/MerchantRepository.java
+++ src/main/java/com/gomcc/merchant_checker_service/repository/MerchantRepository.java
@@ -2,11 +2,24 @@ package com.gomcc.merchant_checker_service.repository;
 
 import com.gomcc.merchant_checker_service.model.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 
-    // add @Query block to fuzzy lookup based on merchant_name
+    /*
+        fuzzy lookup based on merchant_name
+        @partialSearchWord:
+         - `mac` -> `macdonald`
+         - `bee` -> `jollibee`
+         - `kentucky` -\> `kfc`
+     */
+
+    @Query(value = "SELECT mcc, name, description FROM merchant" +
+            " where name ilike :partialSearchWord", nativeQuery = true)
+    Merchant findMerchantDetailsByName(@Param("searchWord") String partialSearchWord);
+
 
 }
